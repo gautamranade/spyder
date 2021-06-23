2d42b2d520be198c91289796ad5af9556c654328
gautamranade <86297800+gautamranade@users.noreply.github.com>
2021-06-23 09:59:40 +0530

    Add files via upload

---
+++ luck identifier.py
@@ -0,0 +1,10 @@
+import random
+
+
+print ('welcome to luck identifier !')
+
+print ('your luck will be expressed in numbers, out of 100! ')
+
+x = input('enter your first letter-')
+
+print (random.randint(0,100) )  
